49ad2f640e5dce6c1e6239b900a8aeaf4f8ab3b9
deepsingh245 <deepsingh25244@gmail.com>
2025-12-10 14:09:06 +0530

    port added

--- server.js
+++ server.js
@@ -263,6 +263,6 @@ const server = createServer(async (req, res) => {
   }
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0",() => {
   console.log(`Test server running at http://localhost:${PORT}/ (PORT=${PORT})`);
 });
